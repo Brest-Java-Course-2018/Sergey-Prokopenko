22f620b2a2d938782883a5bbb9fd1fe5398b93d4
SergeyProkopenko <sergprokop1305@gmail.com>
2018-02-15 16:17:43 +0300

    Connected to Database and corrected dependencies

---
+++ main-app/src/main/java/com/epam/brest/cource/App.java
@@ -0,0 +1,14 @@
+package com.epam.brest.cource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DBUtils dbUtils = new DBUtils();
+    }
+}
--- src/main/java/com/epam/brest/cource/App.java
+++ src/main/java/com/epam/brest/cource/App.java
@@ -4,10 +4,11 @@ package com.epam.brest.cource;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
 
-        DBUtils dbUtils = new DBUtils();
+
 
     }
 }
