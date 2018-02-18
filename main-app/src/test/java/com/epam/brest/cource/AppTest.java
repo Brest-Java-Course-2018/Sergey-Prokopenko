3b092619f56d9509fc81323f14fa5e63b5b008a8
SergeyProkopenko <sergprokop1305@gmail.com>
2018-02-18 16:47:10 +0300

    Create table, insert, select and delete operations

--- main-app/src/test/java/com/epam/brest/cource/AppTest.java
+++ main-app/src/test/java/com/epam/brest/cource/AppTest.java
@@ -4,35 +4,30 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+
+    public AppTest(String testName)
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+
+    public void testApp() throws SQLException, ClassNotFoundException {
+/*
+        DBUtils dbUtils = new DBUtils();
+        Connection connection = dbUtils.getConnection();
+        dbUtils.addUser(connection, "test1", "test1", "test1");
+
+        assertEquals(1, 1);*/
     }
+
 }
