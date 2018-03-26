61f136c67644f3ff6a046e79ea2e7b9bdf3c34c7
SergeyProkopenko <sergprokop1305@gmail.com>
2018-03-26 21:02:09 +0300

    Client-rest full

---
+++ client-rest/src/main/java/com/epam/brest/course/client/ServerDataAccessException.java
@@ -0,0 +1,12 @@
+package com.epam.brest.course.client;
+
+/**
+ *
+ */
+public class ServerDataAccessException extends RuntimeException {
+
+    public ServerDataAccessException(String message) {
+        super(message);
+    }
+
+}
